01e480025f451801c82beb33a08455791f3fc733
Kishan <prasadkishan2@outlook.com>
2026-06-15 11:19:51 -0600

    Added title to help page

--- EMG-IMU/frontend/src/Pages/Help/Help.jsx
+++ EMG-IMU/frontend/src/Pages/Help/Help.jsx
@@ -6,31 +6,19 @@
 import { Link, useNavigate } from 'react-router-dom'
 import './Help.css'
 import BackButton from '../../Components/BackButton/BackButton'
+import { CardImage } from '../../components'
 
 function Help() {
+
+	
 
 return (
 	<>
 		<section className="help-section">
 			<BackButton to='/'/>
-			<section className='help-flex'>
-				<h1>Extra Help</h1>
-				<h2>Hardware needed</h2>
-				<ul>
-					<li>3 IMUs</li>
-					<li>2 EMGs</li>
-					<li>Camera</li>
-					<li>...</li>
-				</ul>
-				<h2>What should be open</h2>
-				<ul>
-					<li>You should have two terminals open, one for frontend and one for backend.</li>
-					<li>Check which com ports are being used.</li>
-					<li>...</li>
-				</ul>
-				<section className='button-flex'>
-					<a href=""></a>
-				</section>
+			<h1 className='title-card'>Extra Help</h1>
+			<section className='three-flex'>
+				<CardImage />
 			</section>
 		</section>
 	</>
